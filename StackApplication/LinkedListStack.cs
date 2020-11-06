e47e5ba19fe59feb58b3250d756003a00ce32078
Chetan2028 <bmchetan2028@gmail.com>
2020-11-06 22:35:37 +0530

    [Chetan] Add . Added to create a simple stack

--- StackApplication/LinkedListStack.cs
+++ StackApplication/LinkedListStack.cs
@@ -6,5 +6,47 @@ namespace StackApplication
 {
     class LinkedListStack
     {
+        public Node top;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedListStack"/> class.
+        /// </summary>
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        /// <summary>
+        /// Pushes the specified new data.
+        /// </summary>
+        /// <param name="newData">The new data.</param>
+        public void Push(int newData)
+        {
+            Node newNode = new Node(newData);
+            if (top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+            Console.WriteLine("{0} pushed into stack",newData);
+        }
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        public void Display()
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + "  ");
+                temp = temp.next;
+            }
+        }
+
+
     }
 }
